919b323a0a56b0d3a96904f9742799af00151962
Liron <Liron@Liron-PC>
2019-06-26 15:38:07 -0700

    Working game with no score

---
+++ Shoot The Asteroids!/Shoot_The_Asteroids/main.py
@@ -0,0 +1,174 @@
+'''
+Created on Jun 25, 2019
+
+@author: Liron
+'''
+
+import pygame
+import random
+from os import path
+
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
+
+#Colors
+WHITE = (255,255,255)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+
+# Asset folders
+game_folder = path.dirname(__file__)
+img_dir = path.join(game_folder, "img")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img, (50,38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 20
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+    
+    def update (self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        elif keystate[pygame.K_RIGHT]:
+            self.speedx = 5 
+        self.rect.x += self.speedx
+        
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+            
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+            
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_orig = meteor_img
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.radius = int(self.rect.width * .85 / 2)
+        self.speedy = random.randrange(1,8)
+        self.speedx = random.randrange(-3,3)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8,8)
+        self.last_updated = pygame.time.get_ticks()
+        
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_updated > 50:
+            self.last_updated = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+            
+    
+    def update(self):
+        self.rotate()
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1,8)
+            self.speedx = random.randrange(-3,3)
+    
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+        
+    def update(self):
+        self.rect.y += self.speedy
+        
+        #remove bullet if it goes off screen
+        if self.rect.bottom > HEIGHT:
+            self.kill()
+        
+        
+
+#initialize pygame and create window
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shoot_The_Asteroids!")
+clock = pygame.time.Clock()
+
+#Load all game graphics
+background = pygame.image.load(path.join(img_dir, "space_background.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
+meteor_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()
+bullet_img = pygame.image.load(path.join(img_dir, "laserRed16.png")).convert()
+
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+running = True
+while running:
+    clock.tick(FPS)
+    
+    #Process events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+    
+    #Update
+    all_sprites.update()
+    mobs.update()
+    bullets.update()
+    
+    #Check to see if a bullet hit an enemy
+    hits = pygame.sprite.groupcollide(bullets, mobs, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+    
+    #Check to see if a mob hit the player
+    hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
+    if hits:
+        running = False
+    
+    #Display
+    screen.blit(background, background_rect)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+    
+pygame.quit()
